3666ea8c62117a88df67829dd5dd1b2e242a6771
William Wyatt <tsangares@gmail.com>
2024-03-22 17:45:13 -0700

    init

--- main.py
+++ main.py
@@ -4,34 +4,45 @@ import os
 
 # Load environment variables from .env file
 load_dotenv()
-
-SECRET = os.getenv('SECRET', "apple")
-
-
+VIEW_KEY = os.getenv("VIEW_KEY")
 app = Flask(__name__)
 
 ip_list = []
+
+ip_map = {
+    "apple": [ "127.0.0.1" ], 
+}
 
 @app.route('/ip', methods=['POST'])
 def receive_ip():
     data = request.get_json()
     ip = data.get('ip',None)
     secret = data.get('secret', None)
-    if secret != SECRET:
-        return "Invalid request"
-    #print(ip, secret)
     if ip:
         ip_list.append(ip)
+        if secret is not None and ip not in ip_map[secret]:
+            ip_map[secret] = ip
         return f"IP {ip} added to the list."
     else:
         return "No IP provided."
+    
 
 @app.route('/iplist', methods=['GET'])
 def get_ip_list():
+    view_key = request.args.get('view_key',None)
     secret = request.args.get('secret',None)
-    if secret != SECRET:
+    if view_key is None or secret is None or view_key != VIEW_KEY:
         return "Invalid request"
-    return ', '.join(list(set(ip_list)))
+    if secret not in ip_map:
+        return []
+    else:
+        return ip_map[secret]
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6086)
+    params = {
+        'host': str(os.getenv('HOST','localhost')),
+        'port': int(os.getenv('PORT',8888)),
+        'debug': bool(os.getenv('DEBUG',False))
+    }
+    app.run(**params)
